f7b4ae3c32dd775cfe34d29e757e9f8c0ea59f8c
Ramsundar <RA20092582@wipro.com>
2023-01-15 17:29:52 +0530

    Blog initial fetching records from markdown done

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,9 +1,44 @@
 import Layout from "@/components/layout"
+import { GetStaticProps } from "next";
+import Head from "next/head";
+import { getSortedPostsData } from '../../lib/posts';
+import utilStyles from '../../styles/utils.module.css'
 
-export default function BlogHome() {
+type PostDataObject = {
+    date: string
+    title: string
+    id: string
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function BlogHome({allPostsData}: {allPostsData: PostDataObject[]}) {
     return (
         <Layout>
-            Blog Home
+            <Head>
+                <title>Blog</title>
+            </Head>
+            <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+                <h2 className={utilStyles.headingLg}>Blog</h2>
+                <ul className={utilStyles.list}>
+                    {allPostsData.map(({ id, date, title }) => (
+                        <li className={utilStyles.listItem} key={id}>
+                            {title}
+                            <br />
+                            {id}
+                            <br />
+                            {date}
+                        </li>
+                    ))}
+                </ul>
+            </section>
         </Layout>
     )
 }
